232d5392dc4e9f8fe4f917c8c5ea53565ac705f9
Fernando Morais <fernandouitec@gmail.com>
2026-06-22 23:44:38 -0300

    feat: deploy production - angular build + htaccess + index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/backend/public/index.php';
+$_SERVER['SCRIPT_NAME']     = '/index.php';
+$_SERVER['PHP_SELF']        = '/index.php';
+require __DIR__ . '/backend/public/index.php';
